a7f46e018ccd6ca678d20c6f4db1847327ad2860
MalgorzataBorowy <61415965+MalgorzataBorowy@users.noreply.github.com>
2020-07-29 14:18:13 +0200

    Editing records

--- LatchApp/ViewData.cs
+++ LatchApp/ViewData.cs
@@ -32,10 +32,13 @@ namespace LatchApp
                         connection.Open();
                         using (MySqlDataReader dataReader = command.ExecuteReader())
                         {
+                            dataGridView1.DataSource = null;
                             DataTable dataTable = new DataTable();
                             dataTable.Load(dataReader);
                             this.dataGridView1.DataSource = dataTable;
+                            dataGridView1.AutoResizeColumns();
                             dataReader.Close();
+                            dataGridView1.Columns[0].ReadOnly = true;
                         }
                     }
                     catch
@@ -119,7 +122,73 @@ namespace LatchApp
         }
         private void BtnSubmitChanges_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Message");
+            foreach (DataGridViewRow oneRow in dataGridView1.Rows)
+            {
+                if (oneRow.Cells[0].Value != null)
+                {
+                    string queryString = "", componentId = "", type = "", status = "", latchId = "", testId = "", endTime = "", date = "", cycles = "", videoLink = "", comments = "";
+                    switch(table)
+                    {
+                        case "components":
+                            componentId = oneRow.Cells[0].Value.ToString();
+                            type = oneRow.Cells[1].Value.ToString();
+                            status = oneRow.Cells[2].Value.ToString().ToLower();
+                            queryString = $"UPDATE sql_latch_tests1.components SET type = '{type}', status = {status} WHERE component_id = {componentId};";
+                            break;
+
+                        case "latches":
+                            latchId = oneRow.Cells[0].Value.ToString();
+                            type = oneRow.Cells[1].Value.ToString();
+                            componentId = oneRow.Cells[2].Value.ToString();
+                            queryString = $"UPDATE sql_latch_tests1.latches SET type = '{type}', component_id = {componentId} WHERE latch_id = {latchId};";
+                            break;
+                        case "tests":
+                            testId = oneRow.Cells[0].Value.ToString();
+                            latchId = oneRow.Cells[1].Value.ToString();
+                            endTime = oneRow.Cells[2].Value.ToString();
+                            date = oneRow.Cells[3].Value.ToString();
+                            status = oneRow.Cells[4].Value.ToString().ToLower();
+                            cycles = oneRow.Cells[5].Value.ToString();
+                            videoLink = oneRow.Cells[6].Value.ToString();
+                            comments = oneRow.Cells[7].Value.ToString();
+                            queryString = $"UPDATE sql_latch_tests1.tests SET latch_id = {latchId}, end_time = '{endTime}', date = '{date}', status = {status}, cycles = {cycles}, video_link = '{videoLink}', comments = '{comments}' WHERE test_id = {testId};";
+                            break;
+                    }
+
+
+                    using (MySqlConnection connection = new MySqlConnection(Properties.Settings.Default.connString))
+                    {
+                        try
+                        {
+                            connection.Open();
+                            using (MySqlCommand command = new MySqlCommand(queryString, connection))
+                            {
+                                command.ExecuteNonQuery();
+                            }
+                        }
+                        catch (MySqlException ex)
+                        {
+                            if (ex.Message.ToString().ToLower().Contains("foreign") && table == "latches")
+                            {
+                                MessageBox.Show("Element does not exist.");
+                            }
+                            else if (ex.Message.ToString().ToLower().Contains("foreign") && table == "tests")
+                            {
+                                MessageBox.Show("Latch does not exist or multiple tests for the same latch id.");
+                            }
+                            else
+                            {
+                                MessageBox.Show(ex.Message.ToString());
+                            }
+                        }
+                        finally
+                        {
+                            connection.Close();
+                        }
+                    }
+                }
+
+            }
         }
 
         private void BtnFinishView_Click(object sender, EventArgs e)
